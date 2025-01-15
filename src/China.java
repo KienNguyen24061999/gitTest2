56ea2515aab9d53d82fd18906d9a345c40053eb0
Kiennguyen2406 <155146504+Kiennguyen2406@users.noreply.github.com>
2025-01-15 10:08:12 +0700

    push test thu

---
+++ src/China.java
@@ -0,0 +1,15 @@
+public class China {
+    private String name;
+
+    public China(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
