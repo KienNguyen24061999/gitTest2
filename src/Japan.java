21ba9d1c8d4cdfd113aecb6f82cc61b03a294df1
Kiennguyen2406 <155146504+Kiennguyen2406@users.noreply.github.com>
2025-01-15 10:02:37 +0700

    KK

---
+++ src/Japan.java
@@ -0,0 +1,14 @@
+public class Japan {
+    private String name;
+    public Japan(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
